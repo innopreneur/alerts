fcb8356f1901ff4edfa419f7ba8fecce3ce52adb
Manan Patel <manan77patel@gmail.com>
2023-02-28 17:21:13 +0530

    +velo price tracker

---
+++ contracts.js
@@ -0,0 +1,10 @@
+import VelodromeRouterABI from './abi/velodrome-router.json'
+
+export default {
+  VELODROME_ROUTER: {
+    abi: VelodromeRouterABI,
+    address: '0x9c12939390052919af3155f41bf4160fd3666a6f',
+    chainId: 10,
+    rpc: 'https://mainnet.optimism.io',
+  },
+}
