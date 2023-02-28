8af73b164bc2c9631ae915f42b4c21eac55b65df
Manan Patel <manan77patel@gmail.com>
2023-02-28 22:31:59 +0530

    + velo price alert script

--- src/velo.js
+++ src/velo.js
@@ -11,10 +11,14 @@ console.log(discordWebhook)
 
 async function checkPrices() {
   const priceResp = await axios.get(coingeckoUrl)
+  console.log('resp', priceResp.status)
   if (priceResp.status == 200) {
+    console.log('data', priceResp.data[veloTokenAddress])
     if (priceResp.data[veloTokenAddress]?.usd) {
       const price = Number(priceResp.data[veloTokenAddress]?.usd)
-      if (targetPrice >= price) {
+      console.log('price : ', price)
+      if (price >= targetPrice) {
+        console.log('Price target triggered... Sending message')
         const message = {
           token: '$VELO',
           price,
@@ -26,9 +30,8 @@ async function checkPrices() {
           content: `@everyone ${JSON.stringify(message)}`,
         }
 
-        await axios.post(discordWebhook, JSON.stringify(params), {
+        axios.post(discordWebhook, JSON.stringify(params), {
           headers: { 'Content-type': 'application/json' },
-          s,
         })
       }
     }
