38248cb662cb0ceb29ac367ce9387fbc1c2adbf1
Manan Patel <manan77patel@gmail.com>
2023-02-28 21:36:51 +0530

    + velo price alert script

--- src/velo.js
+++ src/velo.js
@@ -1,1 +1,38 @@
+import dotenv from 'dotenv'
 import axios from 'axios'
+
+dotenv.config()
+const veloTokenAddress = '0x3c8b650257cfb5f272f799f5e2b4e65093a11a05'
+const discordWebhook = process.env.DISCORD_WEBHOOK_URL
+const targetPrice = Number(process.env.TARGET_PRICE)
+const coingeckoUrl =
+  'https://api.coingecko.com/api/v3/simple/token_price/optimistic-ethereum?contract_addresses=0x3c8b650257cfb5f272f799f5e2b4e65093a11a05&vs_currencies=usd'
+console.log(discordWebhook)
+
+async function checkPrices() {
+  const priceResp = await axios.get(coingeckoUrl)
+  if (priceResp.status == 200) {
+    if (priceResp.data?.veloTokenAddress?.usd) {
+      const price = Number(priceResp.data[veloTokenAddress]?.usd)
+      if (targetPrice >= price) {
+        const message = {
+          token: '$VELO',
+          price,
+        }
+
+        const params = {
+          username: 'Alerts',
+          avatar_url: '',
+          content: `@everyone ${JSON.stringify(message)}`,
+        }
+
+        await axios.post(discordWebhook, JSON.stringify(params), {
+          headers: { 'Content-type': 'application/json' },
+          s,
+        })
+      }
+    }
+  }
+}
+
+checkPrices()
