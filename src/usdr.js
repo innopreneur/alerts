65b3fc5cc133dcf28985fc2229374a46ad5afbf2
Manan Patel <manan77patel@gmail.com>
2023-10-14 12:49:41 +0200

    Update usdr.js

--- src/usdr.js
+++ src/usdr.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 dotenv.config()
 const usdrTokenAddress = '0x40379a439d4f6795b6fc9aa5687db461677a2dba'
 const discordWebhook = process.env.DISCORD_WEBHOOK_URL
-const targetPrice = Number(process.env.TARGET_PRICE)
+const targetAbovePrice = Number(process.env.TARGET_ABOVE_PRICE)
+const targetBelowPrice = Number(process.env.TARGET_BELOW_PRICE)
 const coingeckoUrl =
   'https://api.coingecko.com/api/v3/simple/token_price/polygon-pos?contract_addresses=0x40379a439d4f6795b6fc9aa5687db461677a2dba&vs_currencies=usd'
 
@@ -27,10 +28,15 @@ async function checkPrices() {
     if (priceResp.data[usdrTokenAddress]['usd']) {
       const price = Number(priceResp.data[usdrTokenAddress]['usd'])
       console.log('price : ', price)
-      if (price >= targetPrice) {
-        console.log('Price target triggered... Sending message')
+      if (price >= targetAbovePrice) {
+        console.log('Above Price target triggered... Sending message')
         const message = {
-          token: '$USDR',
+          token: '$USDR Above',
+          price,
+        } else if (price <= targetBelowPrice) {
+        console.log('Below Price target triggered... Sending message')
+        const message = {
+          token: '$USDR Below',
           price,
         }
 
