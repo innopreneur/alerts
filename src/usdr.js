34ec5f718b3b1862b8335a5e27336b170126d2b5
Manan Patel <manan77patel@gmail.com>
2023-10-14 12:37:45 +0200

    Update and rename velo.js to usdr.js

--- src/usdr.js
+++ src/usdr.js
@@ -2,20 +2,20 @@ import dotenv from 'dotenv'
 import axios from 'axios'
 
 dotenv.config()
-const veloTokenAddress = '0x3c8b650257cfb5f272f799f5e2b4e65093a11a05'
+const usdrTokenAddress = '0x40379a439d4f6795b6fc9aa5687db461677a2dba'
 const discordWebhook = process.env.DISCORD_WEBHOOK_URL
 const targetPrice = Number(process.env.TARGET_PRICE)
 const coingeckoUrl =
-  'https://api.coingecko.com/api/v3/simple/token_price/optimistic-ethereum?contract_addresses=0x3c8b650257cfb5f272f799f5e2b4e65093a11a05&vs_currencies=usd'
+  'https://api.coingecko.com/api/v3/simple/token_price/polygon-pos?contract_addresses=0x40379a439d4f6795b6fc9aa5687db461677a2dba&vs_currencies=usd'
 
 
 async function checkPrices() {
   const priceResp = await axios.get(coingeckoUrl)
   console.log('resp', priceResp.status)
   if (priceResp.status == 200) {
-    console.log('data', priceResp.data[veloTokenAddress])
-    if (priceResp.data[veloTokenAddress]['usd']) {
-      const price = Number(priceResp.data[veloTokenAddress]['usd'])
+    console.log('data', priceResp.data[usdrTokenAddress])
+    if (priceResp.data[usdrTokenAddress]['usd']) {
+      const price = Number(priceResp.data[usdrTokenAddress]['usd'])
       console.log('price : ', price)
       if (price >= targetPrice) {
         console.log('Price target triggered... Sending message')
